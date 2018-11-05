56ecedff72f6a61c3276d9d4f51c846237e4dc34
Liv-Randi Lersveen <livrandl@stud.ntnu.no>
2018-11-05 19:49:08 +0100

    endringer i javascript og mal; har lagt til div for mobil meny

--- P3/prosjekt_webtek/visArrangement.js
+++ P3/prosjekt_webtek/visArrangement.js
@@ -20,28 +20,3 @@ function linktilarrangement(xml) {
     innhold.append(temp);
   }
 }
-
-//* http://folk.ntnu.no/livrandl/prosjekt_webtek/visArrangement.html?arrangement=Fuglequiz  hente det etter "=" *//
-if((window.location.href).indexOf('?') != -1) {
-    //* http://www.something.hu/#/ask?kerdesPost=fdasdas%20ad%20asd%20ad%20asdas*//
-    var queryString = (window.location.href).substr((window.location.href).indexOf('?') + 1);
-    // "queryString" inneholder arrangement=Fuglequiz
-    var valgt_arrangement = (queryString.split('='))[1];
-    // "valgt_arrangement inneholder Fuglequiz *//
-  }
-
-  function visarrangement(xml){
-    var xmlDoc = xml.responseXML;
-    innhold = document.getElementById("arrangementer")
-    for (var x = 0; x<=3; x++){
-      if (valgt_arrangement == xmlDoc.getElementsByTagName("navn")[x].childNodes[0].nodeValue)
-      const temp = document.createElement("a");
-
-      let navn = xmlDoc.getElementsByTagName("navn")[x].childNodes[0].nodeValue;
-      let dato = xmlDoc.getElementsByTagName("dato")[x].childNodes[0].nodeValue;
-      temp.innerText = dato + " " +navn ;
-      temp.style = "display:block;margin-bottom:5px;";
-      temp.href = "visArrangement.html?arrangement=" +navn;
-      innhold.append(temp);
-
-  }
