e61cf592ae23ed2cc54346415f108ef391c387f1
unknown <morten.mikalsen@hotmail.com>
2018-11-11 23:36:49 +0100

    fjernet logene fra hoyde.js

--- P3/scripts/hoyde.js
+++ P3/scripts/hoyde.js
@@ -1,8 +1,6 @@
 function endreHoeyde(className) {
   totalHoyde = document.getElementsByTagName("HTML")[0].offsetHeight;
   deviceHoyde = document.documentElement.clientHeight;
-  console.log("Total høyde er " + totalHoyde);
-  console.log("Device høyde er " + deviceHoyde);
   if (totalHoyde < deviceHoyde){
     summer = 0;
     element = document.getElementsByClassName(className)[0]
@@ -13,6 +11,5 @@ function endreHoeyde(className) {
     padding_bottom = parseInt(padding_bottom, 10);
     padding = padding_top + padding_bottom;
     element.style.height = (deviceHoyde+intersectionHoyde-totalHoyde-padding) + "px";
-    console.log("Element høyde er  " + element.style.height);
   }
 }
