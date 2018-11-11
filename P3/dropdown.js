4596a0ecc65d304b0636a30b7b94d554821b079b
unknown <morten.mikalsen@hotmail.com>
2018-11-11 13:25:39 +0100

    Endret på mobilmenyen og la til scroll og klikk funksjon i dropdown.js. Endret på fugleoversikt.html

--- P3/dropdown.js
+++ P3/dropdown.js
@@ -1,8 +1,28 @@
+var x = document.getElementById("mobBar");
+const y = document.getElementById("mobilknapp");
+
 function dropdown(){
-  var x = document.getElementById("navBar");
-if (x.style.display === "none") {
+  if (x.style.display === "none") {
     x.style.display = "block";
-} else {
+    // Koden er fra https://www.blustemy.io/detecting-a-click-outside-an-element-in-javascript/
+    document.addEventListener("click",  (evt) => {
+      let targetElement = evt.target; // clicked element
+      do {
+          if (targetElement == x || targetElement == y) {
+            return
+          }
+          // Go up the DOM
+          targetElement = targetElement.parentNode;
+      } while (targetElement);
+
+      // This is a click outside.
+      x.style.display = "none";
+    });
+  } else {
     x.style.display = "none";
+  }
 }
-}
+
+window.onscroll = function() {
+  x.style.display = "none";
+};
