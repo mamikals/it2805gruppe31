36fe36ddf9044e202bb25be6a871f70d3d227d13
MikalBN <mikalbnorvoll@gmail.com>
2018-11-11 18:58:43 +0100

    style changes fugleGalleri

--- P3/fugleinfo.js
+++ P3/fugleinfo.js
@@ -11,20 +11,18 @@ fuglXML.send();
 var url_string = window.location.href; //window.location.href
 var url = new URL(url_string);
 var c = url.searchParams.get("fugl");
-console.log(c);
 
 function hentFugl(xml) {
   var xmlDoc = xml.responseXML;
-  console.log(xmlDoc)
   content = document.getElementById("fugleliste")
   var len = xmlDoc.getElementsByTagName("fugl").length-1;
   for (var x = 0; x <= len; x++) {
     let navn = xmlDoc.getElementsByTagName("navn")[x].childNodes[0].nodeValue;
-    console.log(navn)
     if (navn.toUpperCase() == c.toUpperCase()) {
       document.getElementById('fuglTitle').innerText = xmlDoc.getElementsByTagName("navn")[x].childNodes[0].nodeValue;
       document.getElementById('fuglInfo').innerText = xmlDoc.getElementsByTagName("tekst")[x].childNodes[0].nodeValue;
       document.getElementById('fugleInfoImg').src = xmlDoc.getElementsByTagName("bilde")[x].childNodes[0].nodeValue;
+      document.title = navn;
     }
   }
 
